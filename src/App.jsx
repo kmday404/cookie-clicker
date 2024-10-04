e7af0bed7f79927abd311abb189f82f71353a750
Katherine Day <kmday125@gmail.com>
2024-10-04 16:38:19 +0100

    cookie counter added and functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import "./App.css";
 
+// import { useEffect, useState } from "react";
+import CookieCounter from "./components/CookieCounter";
+
 export default function App() {
+  // useEffect(() => {
+  //   const cookiesPerSecondInterval = setInterval(() => {
+  //     setCookies((myCookieCount) => myCookieCount + cookiesPerSecond);
+  //   }, 1000);
+  //   return () => {
+  //     clearInterval(cookiesPerSecondInterval);
+  //   };
+  // }, [cookiesPerSecond]);
+
   return (
     <>
       <h1>Cookie Clicker</h1>
+      <CookieCounter />
+      {/* <div>Cookies Per Second: {cookiesPerSecond}</div> */}
     </>
   );
 }
---
+++ src/components/CookieCounter.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import cookie from "../../public/images/cookie.png";
+
+export default function CookieCounter() {
+  const [myCookieCount, setMyCookierCount] = useState(0);
+
+  function handleClick() {
+    setMyCookierCount(myCookieCount + 1);
+  }
+  return (
+    <>
+      <img src={cookie} alt="cartoon cookie image" onClick={handleClick} />
+
+      <p>{myCookieCount} cookies.</p>
+    </>
+  );
+}
