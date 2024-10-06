5ef700bbc33e367f464c1de4168fa9c0324ed42f
Katherine Day <kmday125@gmail.com>
2024-10-06 10:53:16 +0100

    moved some items to App.jsx to declare globally (i think)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import "./App.css";
 
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import CookieCounter from "./components/CookieCounter";
 import UpgradesContainer from "./components/UpgradesContainer";
 import upgradeData from "./lib/upgradeData.json";
 
 export default function App() {
+  const [cookies, setCookies] = useState(0);
+  const [cookiesPerSecond, setCookiesPerSecond] = useState(1);
+  // const [myCookieCount, setMyCookierCount] = useState(0);
   return (
     <>
       <h1>Cookie Clicker</h1>
-      <CookieCounter />
+      <CookieCounter
+        cookies={cookies}
+        setCookies={setCookies}
+        cookiesPerSecond={cookiesPerSecond}
+      />
       {upgradeData.map((shop) => {
         return (
           <div key={shop.id}>
@@ -21,6 +28,7 @@ export default function App() {
           </div>
         );
       })}
+      <p>{cookies}</p>
     </>
   );
 }
--- src/components/CookieCounter.jsx
+++ src/components/CookieCounter.jsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from "react";
 import cookie from "../../public/images/cookie.png";
 
-export default function CookieCounter() {
-  const [cookies, setCookies] = useState(0);
-  const [cookiesPerSecond, setCookiesPerSecond] = useState(1);
+export default function CookieCounter({
+  cookies,
+  setCookies,
+  cookiesPerSecond,
+}) {
+  //parent is passed to child ...? moved to App.jsx
+  // const [cookies, setCookies] = useState(0);
+  // const [cookiesPerSecond, setCookiesPerSecond] = useState(1);
   useEffect(() => {
     const cookiesPerSecondInterval = setInterval(() => {
       setCookies((myCookieCount) => myCookieCount + cookiesPerSecond);
